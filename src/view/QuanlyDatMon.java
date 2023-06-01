c57024c909e91e580dfb9e11e1882becb8b5b9cb
tntn245 <tranngoctonhu2405@gmail.com>
2023-06-01 22:15:48 +0700

    add JoptionPane for quanlydatmon

--- src/view/QuanlyDatMon.java
+++ src/view/QuanlyDatMon.java
@@ -38,111 +38,115 @@ import javax.swing.table.TableColumnModel;
  * @author My PC
  */
 public class QuanlyDatMon {
+
     public JPanel pane_bg;
-    
+
     private JPanel pane_QLDM;
     private DropShadowPane pane_shadow;
-    
+
     private JComboBox combobox_Ban;
     private JTable table_DM;
     private JScrollPane Scrollpane_TableDM;
-    
+    private JOptionPane option;
+
     private String SOHD;
     private Connection connection;
-    
-    public QuanlyDatMon(Connection connection){
+
+    public QuanlyDatMon(Connection connection) {
         this.connection = connection;
         init_pane();
     }
-    public void init_pane(){
+
+    public void init_pane() {
         pane_bg = new JPanel();
         pane_bg.setOpaque(true);
         pane_bg.setBackground(new Color(230, 235, 240));
 
-        pane_bg.setPreferredSize(new Dimension(800, 600)); 
-        
+        pane_bg.setPreferredSize(new Dimension(800, 600));
+
         pane_shadow = new DropShadowPane(4, 0, 0, 0, Color.white, 50);
-        pane_shadow.setPreferredSize(new Dimension(770, 550)); 
-        
+        pane_shadow.setPreferredSize(new Dimension(770, 550));
+
         pane_QLDM = new JPanel();
         pane_QLDM.setBackground(Color.white);
-        pane_QLDM.setPreferredSize(new Dimension(760, 540)); 
+        pane_QLDM.setPreferredSize(new Dimension(760, 540));
         pane_QLDM.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
-        
+
         combobox_Ban = new JComboBox();
         combobox_Ban.setPreferredSize(new Dimension(700, 35));
-        for(int i=0; i<15;i++){
-            int SoBan = i+1;
-            combobox_Ban.addItem("Bàn "+SoBan);
+        for (int i = 0; i < 15; i++) {
+            int SoBan = i + 1;
+            combobox_Ban.addItem("Bàn " + SoBan);
         }
         table_DM();
         add_data_table(combobox_Ban.getSelectedItem());
         combobox_Ban.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) { 
+            public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED) {
                     add_data_table(combobox_Ban.getSelectedItem());
                 }
             }
         });
-                
+
+        option = new JOptionPane();
+
         pane_QLDM.add(combobox_Ban);
         pane_QLDM.add(Scrollpane_TableDM);
         pane_shadow.add(pane_QLDM);
         pane_bg.add(pane_shadow);
-    }    
-    
-    private void table_DM(){
-        Scrollpane_TableDM= new JScrollPane();
+    }
+
+    private void table_DM() {
+        Scrollpane_TableDM = new JScrollPane();
 
         String[] columnNames = {"Mã món", "Tên món", "SL", "Tình trạng", ""};
         DefaultTableModel model = new DefaultTableModel(columnNames, 0) {
-            boolean[] canEdit = new boolean [] {
+            boolean[] canEdit = new boolean[]{
                 false, false, false, false, true
             };
-            
+
             @Override
             public boolean isCellEditable(int row, int column) {
-               return canEdit [column];
+                return canEdit[column];
             }
         };
         table_DM = new JTable(model);
         table_DM.setRowHeight(40);
-        table_DM.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
+        table_DM.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
             @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
-            {
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 final Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                c.setBackground(row % 2 == 0 ? new Color(234,247,255) : new Color(255, 237, 243));
+                c.setBackground(row % 2 == 0 ? new Color(234, 247, 255) : new Color(255, 237, 243));
                 c.setForeground(Color.black);
                 return c;
             }
-        });        
-        
+        });
+
         table_DM.getTableHeader().setOpaque(false);
-        table_DM.getTableHeader().setBackground(new Color(167,222,254));
+        table_DM.getTableHeader().setBackground(new Color(167, 222, 254));
         table_DM.getTableHeader().setForeground(Color.black);
         table_DM.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 14));
 //        table_NV.getTableHeader().setFont(new Font(table_NV.getFont().getName(),Font.BOLD,14));
-        table_DM.getTableHeader().setPreferredSize(new Dimension(table_DM.getWidth(),40));
+        table_DM.getTableHeader().setPreferredSize(new Dimension(table_DM.getWidth(), 40));
         table_DM.getTableHeader().setBorder(BorderFactory.createMatteBorder(1, 0, 1, 0, Color.black));
-                
+
         table_DM.setShowHorizontalLines(false);
         table_DM.setGridColor(Color.white);
         table_DM.setBackground(Color.white);
-        table_DM.setBorder(new EmptyBorder(5, 5, 5,5));
+        table_DM.setBorder(new EmptyBorder(5, 5, 5, 5));
         table_DM.setFont(new Font("SansSerif", Font.PLAIN, 14));
 //        table_NV.setFont(new Font(table_NV.getFont().getName(),Font.PLAIN,14));
-        
+
         TableColumnModel columnModel = table_DM.getColumnModel();
         columnModel.getColumn(0).setPreferredWidth(100);
         columnModel.getColumn(1).setPreferredWidth(150);
         columnModel.getColumn(2).setPreferredWidth(100);
         columnModel.getColumn(3).setPreferredWidth(100);
-        
+
         table_DM.setPreferredScrollableViewportSize(table_DM.getPreferredSize());
         table_DM.setFillsViewportHeight(true);
 //        table_NV.setSelectionBackground(new Color(0, 0, 0, 0));
-        
+
         TableActionEvent event = new TableActionEvent() {
             @Override
             public void onEdit(int row) {
@@ -154,42 +158,43 @@ public class QuanlyDatMon {
                 ChuaLenMon(row);
             }
         };
-        table_DM.getColumnModel().getColumn(4).setCellRenderer(new TableActionCellRender(new Color(234,247,255), new Color(255, 237, 243),true, true, "/image/check.png", "/image/multiply.png"));
-        table_DM.getColumnModel().getColumn(4).setCellEditor(new TableActionCellEditor(event, new Color(234,247,255), new Color(255, 237, 243),true,true,"/image/check.png", "/image/multiply.png"));
-        
+        table_DM.getColumnModel().getColumn(4).setCellRenderer(new TableActionCellRender(new Color(234, 247, 255), new Color(255, 237, 243), true, true, "/image/check.png", "/image/multiply.png"));
+        table_DM.getColumnModel().getColumn(4).setCellEditor(new TableActionCellEditor(event, new Color(234, 247, 255), new Color(255, 237, 243), true, true, "/image/check.png", "/image/multiply.png"));
+
         Scrollpane_TableDM.setViewportView(table_DM);
-        Scrollpane_TableDM.setBorder(new LineBorder( Color.LIGHT_GRAY, 1, true));
+        Scrollpane_TableDM.setBorder(new LineBorder(Color.LIGHT_GRAY, 1, true));
         Scrollpane_TableDM.setPreferredSize(new Dimension(700, 420));
     }
-    
-    public void add_data_table(Object TenBan){
+
+    public void add_data_table(Object TenBan) {
         String SoBan = TenBan.toString().substring(4);
         int So = Integer.parseInt(SoBan);
-        DefaultTableModel tbmodel = (DefaultTableModel)table_DM.getModel();
+        DefaultTableModel tbmodel = (DefaultTableModel) table_DM.getModel();
         tbmodel.setRowCount(0);
 
-        try{
-            String sql_SOHD = "SELECT SOHD FROM HOADON WHERE TINHTRANGTHANHTOAN = 0 AND MABAN = 'BAN"+So+"'";
+        try {
+            String sql_SOHD = "SELECT SOHD FROM HOADON WHERE TINHTRANGTHANHTOAN = 0 AND MABAN = 'BAN" + So + "'";
             Statement statement_SOHD = connection.createStatement();
             ResultSet res_SOHD = statement_SOHD.executeQuery(sql_SOHD);
 
-            while(res_SOHD.next()){
+            while (res_SOHD.next()) {
                 SOHD = res_SOHD.getString("SOHD");
-                
-                String sql_CTHD = "SELECT * FROM CTHD WHERE SOHD = '"+SOHD+"'";
+
+                String sql_CTHD = "SELECT * FROM CTHD WHERE SOHD = '" + SOHD + "'";
                 Statement statement_CTHD = connection.createStatement();
                 ResultSet res_CTHD = statement_CTHD.executeQuery(sql_CTHD);
-                
-                while(res_CTHD.next()){
+
+                while (res_CTHD.next()) {
                     String MAMON = res_CTHD.getString("MAMON");
                     String SL = res_CTHD.getString("SL");
                     int TINHTRANGMON = res_CTHD.getInt("TINHTRANGMON");
                     String StrTinhTrang;
-                    if(TINHTRANGMON == 0)
+                    if (TINHTRANGMON == 0) {
                         StrTinhTrang = "Chưa lên món";
-                    else
+                    } else {
                         StrTinhTrang = "Đã lên món";
-                    
+                    }
+
                     String sql_TENMON = "SELECT TENMON FROM MONAN WHERE MAMON = '" + MAMON + "'";
                     Statement statement_TENMON = connection.createStatement();
                     ResultSet res_TENMON = statement_TENMON.executeQuery(sql_TENMON);
@@ -201,9 +206,8 @@ public class QuanlyDatMon {
                     }
                 }
             }
-        }
-        catch(SQLException | HeadlessException ex){
-                    System.out.println("the error is"+ex);
+        } catch (SQLException | HeadlessException ex) {
+            System.out.println("the error is" + ex);
         }
     }
 
@@ -213,16 +217,20 @@ public class QuanlyDatMon {
         }
         DefaultTableModel model = (DefaultTableModel) table_DM.getModel();
         Object value_MAMON = model.getValueAt(row, 0);
-        
+
         try {
             Statement statement = connection.createStatement();
-            
-                String sql = "UPDATE CTHD SET TINHTRANGMON = 1 WHERE SOHD = '" + SOHD + "' AND MAMON = '"+value_MAMON+"'";
-                int res = statement.executeUpdate(sql);
-                
-                model.setValueAt("Đã lên món", row, 3);
+
+            String sql = "UPDATE CTHD SET TINHTRANGMON = 1 WHERE SOHD = '" + SOHD + "' AND MAMON = '" + value_MAMON + "'";
+            int res = statement.executeUpdate(sql);
+
+            model.setValueAt("Đã lên món", row, 3);
+            option.setVisible(true);
+            option.showMessageDialog(pane_bg, "Cập nhật tình trạng lên món thành công!");
         } catch (SQLException ex) {
             Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            option.setVisible(true);
+            option.showMessageDialog(pane_bg, "Có lỗi xảy ra, không thể cập nhật tình trạng lên món!");
         }
     }
 
@@ -232,16 +240,20 @@ public class QuanlyDatMon {
         }
         DefaultTableModel model = (DefaultTableModel) table_DM.getModel();
         Object value_MAMON = model.getValueAt(row, 0);
-        
+
         try {
             Statement statement = connection.createStatement();
-            
-                String sql = "UPDATE CTHD SET TINHTRANGMON = 0 WHERE SOHD = '" + SOHD + "' AND MAMON = '"+value_MAMON+"'";
-                int res = statement.executeUpdate(sql);
-                
-                model.setValueAt("Chưa lên món", row, 3);
+
+            String sql = "UPDATE CTHD SET TINHTRANGMON = 0 WHERE SOHD = '" + SOHD + "' AND MAMON = '" + value_MAMON + "'";
+            int res = statement.executeUpdate(sql);
+
+            model.setValueAt("Chưa lên món", row, 3);
+            option.setVisible(true);
+            option.showMessageDialog(pane_bg, "Cập nhật tình trạng lên món thành công!");
         } catch (SQLException ex) {
             Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            option.setVisible(true);
+            option.showMessageDialog(pane_bg, "Có lỗi xảy ra, không thể cập nhật tình trạng lên món!");
         }
     }
 }
